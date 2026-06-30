8443c9867610372e18e38b95024fb61877b15cd4
Ilkka Lehtoranta <ilkka.lehtoranta@valmet.com>
2026-06-30 21:06:38 +0300

    Optimize encoded track bit reads

--- CopperDisk/AmigaEncodedTrack.cs
+++ CopperDisk/AmigaEncodedTrack.cs
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2026 Ilkka Lehtoranta
+ * SPDX-License-Identifier: MIT
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -189,6 +194,31 @@ public readonly struct AmigaEncodedTrack : IAmigaTrack
 
         var span = EncodedData.Span;
         bitOffset = WrapBitOffset(bitOffset, BitLength);
+        var endBit = bitOffset + bitCount;
+        if (bitCount == 0)
+        {
+            return 0;
+        }
+
+        if (endBit <= BitLength)
+        {
+            var byteOffset = bitOffset >> 3;
+            var bitShift = bitOffset & 7;
+            var byteCount = (bitShift + bitCount + 7) >> 3;
+            if (byteCount <= 8)
+            {
+                var window = 0ul;
+                for (var index = 0; index < byteCount; index++)
+                {
+                    window = (window << 8) | span[byteOffset + index];
+                }
+
+                var shift = (byteCount * 8) - bitShift - bitCount;
+                window >>= shift;
+                return bitCount == 64 ? window : window & ((1ul << bitCount) - 1);
+            }
+        }
+
         var value = 0ul;
         for (var bit = 0; bit < bitCount; bit++)
         {
